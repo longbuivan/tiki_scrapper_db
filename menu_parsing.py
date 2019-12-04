842e27b2708e97b211ad466b6259a1c34fc1caf0
dragon <45690915+longbuivan@users.noreply.github.com>
2019-12-04 18:22:05 +0700

    Update menu_parsing.py

--- menu_parsing.py
+++ menu_parsing.py
@@ -2,19 +2,17 @@ from bs4 import BeautifulSoup
 import re
 from urllib.request import urlopen
 
+
 all_list = []
 
 def menu_extract(link):
-
     html = urlopen(link)
     bs = BeautifulSoup(html, 'html.parser')
-
+    # extract a list of category
     menu_list = bs.find_all("a", attrs={"class":re.compile('MenuItem_')})
-
     for menu in menu_list:
         all_list.append(menu['href'])
-        # print(menu_list)
     return all_list
-
+# print on when a build done successfully
 print("the ingestion is done, please check file results")
 
